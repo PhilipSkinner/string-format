388b54a3c9d473c8a45cde4eb680ac3834a2fddf
Philip Skinner <me@philip-skinner.co.uk>
2020-05-16 20:27:29 +0100

    [+] Adds more number formatters.

--- spec/allSpec.js
+++ spec/allSpec.js
@@ -79,6 +79,77 @@ const exponentialsTest = (done) => {
 	done();
 };
 
+const genericNumberTest = (done) => {
+	process.env.LANG = 'en_US';
+
+	expect(stringFormat("{0:G}", -123.456)).toEqual("-123.456");
+	expect(stringFormat("{0:G}", -1.234567890e-25)).toEqual("-1.234568E-25");
+	expect(stringFormat("{0:G4}", 123.4546)).toEqual("123.5");
+
+	process.env.LANG = 'sv_SE';
+
+	expect(stringFormat("{0:G}", -123.456)).toEqual("-123,456");
+	expect(stringFormat("{0:G}", -1.234567890e-25)).toEqual("-1,234568E-25");
+	expect(stringFormat("{0:G4}", 123.4546)).toEqual("123,5");
+
+	done();
+};
+
+const numbersTest = (done) => {
+	process.env.LANG = 'en_US';
+
+	expect(stringFormat("{0:N}", 1234.567)).toEqual("1,234.57");
+	expect(stringFormat("{0:N1}", 1234)).toEqual("1,234.0");
+	expect(stringFormat("{0:N3}", -1234.56)).toEqual("-1,234.560");
+
+	process.env.LANG = 'sv_SE';
+
+	expect(stringFormat("{0:N}", 1234.567)).toEqual("1.234,57");
+	expect(stringFormat("{0:N1}", 1234)).toEqual("1.234,0");
+	expect(stringFormat("{0:N3}", -1234.56)).toEqual("-1.234,560");
+
+	done();
+};
+
+const percentagesTest = (done) => {
+	process.env.LANG = 'en_US';
+
+	expect(stringFormat("{0:P}", 0.12)).toEqual("12%");
+	expect(stringFormat("{0:P1}", 0.1234)).toEqual("12.3%");
+	expect(stringFormat("{0:P3}", 0.123456)).toEqual("12.346%");
+
+	process.env.LANG = 'sv_SE';
+
+	expect(stringFormat("{0:P}", 0.12)).toEqual("12%");
+	expect(stringFormat("{0:P1}", 0.1234)).toEqual("12,3%");
+	expect(stringFormat("{0:P3}", 0.123456)).toEqual("12,346%");
+
+	done();
+};
+
+const roundTripTest = (done) => {
+	process.env.LANG = 'en_US';
+
+	expect(stringFormat("{0:R}", 0.12)).toEqual("0.12");
+	expect(stringFormat("{0:r}", 0.1234)).toEqual("0.1234");
+
+	process.env.LANG = 'sv_SE';
+
+	expect(stringFormat("{0:R}", 0.12)).toEqual("0.12");
+	expect(stringFormat("{0:r}", 0.1234)).toEqual("0.1234");
+
+	done();
+};
+
+const hexTest = (done) => {
+	process.env.LANG = 'en_US';
+
+	expect(stringFormat("{0:x}", 255)).toEqual("ff");
+	expect(stringFormat("{0:X4}", 255)).toEqual("00FF");
+
+	done();
+};
+
 describe("A string formatter", () => {
 	it("ignores none strings", notStringIgnoreTest);
 	it("can inject variables into a string", variableTest);
@@ -87,4 +158,9 @@ describe("A string formatter", () => {
 	it("can format fixed-point numbers", fixedTest);
 	it("can format decimals", decimalsTest);
 	it("can format exponentials", exponentialsTest);
+	it("can format generic numbers", genericNumberTest);
+	it("can format numbers", numbersTest);
+	it("can format percentages", percentagesTest);
+	it("can do round trip number formats", roundTripTest);
+	it("can format hexadecimals", hexTest);
 });
